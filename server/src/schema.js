ded0d6ed5efc15b1c27152196cb2bb55cc9393a5
HarryGitUser <117655847+HarryGitUser@users.noreply.github.com>
2023-08-13 22:03:09 +0530

    lift 3 updated

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,31 +4,53 @@ const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
+    "Fetch a specific track, provided a track's ID"
+    track(id: ID!): Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
-    id: ID!
+      id: ID!
     "The track's title"
-    title: String!
+      title: String!
     "The track's main Author"
-    author: Author!
+      author: Author!
     "The track's illustration to display in track card or track page detail"
-    thumbnail: String
+      thumbnail: String
     "The track's approximate length to complete, in minutes"
-    length: Int
+      length: Int
     "The number of modules this track contains"
-    modulesCount: Int
+      modulesCount: Int
+"The track's complete description, can be in Markdown format"
+  description: String
+"The number of times a track has been viewed"
+  numberOfViews: Int
+
+"The track's complete array of Modules"
+modules: [Module!]!
   }
 
-  "Author of a complete Track or a Module"
+
+"Author of a complete Track or a Module"
   type Author {
-    id: ID!
+      id: ID!
     "Author's first and last name"
-    name: String!
+      name: String!
     "Author's profile picture"
-    photo: String
+      photo: String
   }
+
+
+"A Module is a single unit of teaching. Multiple Modules compose a Track"
+type Module {
+    id: ID!
+  "The Module's title"
+    title: String!
+  "The Module's length in minutes"
+    length: Int
+}
+
+
 `;
 
 module.exports = typeDefs;
